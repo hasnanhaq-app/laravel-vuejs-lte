7142060c157979ba74ac7b17365b771ff4c5489c
MacBook M1 <macbookm1@bkd-macpro.local>
2022-12-26 15:21:24 +0700

    Integrasi Admin LTE 3 dan Font Awesome menggunakan NPM

---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<head>
+
+    @include('template.head')
+
+    <title>AdminLTE 3 | Starter</title>
+
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        @include('template.navbar')
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        @include('template.left-sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper" id="app">
+
+            <!-- Main content -->
+            <div class="content">
+                <div class="container-fluid">
+                    <example-component></example-component>
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+
+        <!-- Main Footer -->
+        @include('template.footer')
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    @include('template.script')
+</body>
+
+</html>
